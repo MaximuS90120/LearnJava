58b15d28c2cb4049d514e3f10f83c5020ba07ad9
BasttUK <157982398+BasttUK@users.noreply.github.com>
2024-02-14 22:12:39 +0300

    вернул main Захара, перенес метод саморасширения массива в класс AdvanceArray

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,8 @@
 public class Main {
-    Object[] array = new Object[3];
-    int size = 0;
-    int capacity = 3;
-
-    public void addObject(Object AdvancedArray) {
-        if (size == capacity) {
-            capacity += 3;
-            Object[] newArray = new Object[capacity];
-            for (int i = 0; i < size; i++) {
-                newArray[i] = array[i];
-            }
-            array = newArray;
+        public static void main(String[] args) {
+            StringBuffer strBuffer = new StringBuffer("Я люблю пить пиво!");
+            System.out.println(strBuffer.toString());
+            strBuffer.reverse();
+            System.out.println(strBuffer.toString());
         }
-        array[size++] = AdvancedArray;
     }
-
-    public static void main(String[] args) {
-        Main FinArray = new Main();
-        FinArray.addObject("1");
-        FinArray.addObject("2");
-        FinArray.addObject("3");
-        System.out.println(FinArray.size);
-        //System.out.println(main.array[3]);
-        FinArray.addObject("4");
-        System.out.println(FinArray.array[3]);
-        System.out.println(FinArray.array[4]);
-        //System.out.println(main.array[6]);
-    }
-}
